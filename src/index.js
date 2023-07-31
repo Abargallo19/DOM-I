c9eea75fbbb17be6be903f3a5729c7769f7262c8
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-07-31 02:11:10 -0400

    features heading added to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -7,66 +8,79 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
+  contact: {
     "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: "Copyright Great Idea! 2021",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
 
-console.log('project wired!')
-console.log("Hey! Happy to have ya!!")
+console.log("Hey! Happy to have ya!!");
 
 //NAV LINKS
-const links = document.querySelectorAll('nav a');
+const links = document.querySelectorAll("nav a");
 const linkText = Object.values(siteContent.nav);
 
 links.forEach((link, idx) => {
-  link.textContent = linkText[idx]
-  link.classList.add('italic')
-})
+  link.textContent = linkText[idx];
+  link.classList.add("italic");
+});
 
 //LARGE CONTENT
 
-const cta = document.querySelector('.cta');
-const ctaHeader = cta.querySelector('h1');
+const cta = document.querySelector(".cta");
+const ctaHeader = cta.querySelector("h1");
 
-ctaHeader.textContent = siteContent['cta']['h1'];
+ctaHeader.textContent = siteContent["cta"]["h1"];
 
-const ctaButton = cta.querySelector('button');
+const ctaButton = cta.querySelector("button");
 
-ctaButton.textContent = siteContent['cta']['button']
+ctaButton.textContent = siteContent["cta"]["button"];
 
 //IMAGES
-const logoImg = document.querySelector('#logo-img');
-const ctaImg = document.querySelector('#cta-img');
-const accentImg = document.querySelector('#middle-img');
+const logoImg = document.querySelector("#logo-img");
+const ctaImg = document.querySelector("#cta-img");
+const accentImg = document.querySelector("#middle-img");
 
 logoImg.src = "http://localhost:9000/img/logo.png";
 ctaImg.src = "http://localhost:9000/img/cta.png";
 accentImg.src = "http://localhost:9000/img/accent.png";
+
+//TOP CONTENT
+const topContent = document.querySelector(".top-content");
+
+const features = topContent.querySelector(".text-content:nth-of-type(1)");
+features.querySelector("h4").textContent =
+  siteContent["main-content"]["features-h4"];
+
+const about = document.querySelector();
